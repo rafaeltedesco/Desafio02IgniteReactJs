4ab81e10c46370cd643b8a7514e100043d700435
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-03-07 22:07:17 -0300

    refactor

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,40 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard"
+
+import { GenreResponseProps } from "./SideBar";
+
+export interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+
+type ContentProps = {
+  data: {
+    movies: Array<MovieProps>;
+  };
+  selectedGenre: GenreResponseProps;
+}
+
+export function Content({ data: { movies }, selectedGenre }: ContentProps) {
+  return (
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
 }
